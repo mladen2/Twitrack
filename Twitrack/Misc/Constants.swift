85c2ba010713f38c598b52c98dde9d12ffbb8a61
mladen2 <mladen@nishco.net>
2023-10-11 22:03:31 +0100

    Restore Constants with empty API keys (no free twitter access)

---
+++ Twitrack/Misc/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Twitrack
+//
+//  Created by Mladen Nisevic on 10/08/2021.
+//
+
+import Foundation
+
+
+let tweetExpiryPeriod: TimeInterval = 15 // seconds
+var defaultSearchTerms = ["I", "me"]
+//var defaultSearchTerms = ["python"]
+//var defaultSearchTerms = ["spacex"]
+
+let urlCallback = "twitrack://success"
+
+struct TwitterConstant {
+    static let APIKey = ""
+    static let APISecretKey = ""
+    static let bearerToken = ""
+}
